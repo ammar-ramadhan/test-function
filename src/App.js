ee5111193e7086e9d55c9ebb0b7c9c3a85350635
Mohammad Ammar Ramadhan <ammar.ramadhan@zeniuseducation.com>
2023-01-03 11:17:42 +0700

    test fetch api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [apiResponse, setApiResponse] = useState();
+
+  useEffect(() => {
+    fetch("/.netlify/functions/hello")
+      .then((response) => response.text())
+      .then((data) => {
+        setApiResponse(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          API Response: <code>{apiResponse}</code>
         </p>
         <a
           className="App-link"
