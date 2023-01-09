79495fcc51f7ddcbdb6e451c979b34afc8622519
Mohammad Ammar Ramadhan <ammar.ramadhan@zeniuseducation.com>
2023-01-09 12:24:07 +0700

    test get user from event login

--- netlify/functions/identity-login.js
+++ netlify/functions/identity-login.js
@@ -1,8 +1,26 @@
+import fetch, { Headers } from "node-fetch";
+
 exports.handler = async function (event, context) {
   const { identity, user } = context.clientContext;
-  console.log({ identity, user });
+  console.log(context.clientContext);
+  console.log({ event });
   console.log({ context });
+  //   console.log({ identity, user });
+  //   console.log({ context });
+  if (identity) {
+    const token = "Bearer " + identity.token;
+    console.log({ token });
+    const response = await fetch(identity.url + "/user", {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      headers: new Headers({
+        Authorization: token,
+      }),
+    });
+    const result = await response.json();
+    console.log({ result });
+  }
   return {
     statusCode: 200,
+    body: JSON.stringify(context),
   };
 };
