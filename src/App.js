7d60fb455cbfd675e6896d59cadc1334692418e9
Mohammad Ammar Ramadhan <ammar.ramadhan@zeniuseducation.com>
2023-01-11 11:21:15 +0700

    connect to emulator when in dev local

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 // import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 // import firebase from "firebase";
 import { auth } from "firebaseui";
 import firebase from "firebase/compat/app";
@@ -15,19 +15,7 @@ function App() {
   const [products, setProducts] = useState([]);
   const [isSignedIn, setSignedIn] = useState(false);
 
-  // TODO: Replace the following with your app's Firebase project configuration
-  var firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  };
-  firebase.initializeApp(firebaseConfig);
-
-  var authUI = firebase.auth();
+  var authUI = getAuth();
   authUI.onAuthStateChanged(
     function (user) {
       if (user) {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import firebase from "firebase/compat/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+firebase.initializeApp(firebaseConfig);
+
+if (process.env.NODE_ENV === "development") {
+  const auth = getAuth();
+  connectAuthEmulator(auth, "http://localhost:9099");
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
