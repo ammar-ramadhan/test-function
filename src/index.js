af70d0bd83b06bc2092170b058be44a6389b82b6
Mohammad Ammar Ramadhan <ammar.ramadhan@zeniuseducation.com>
2023-01-11 12:20:35 +0700

    update config

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import { getAuth, connectAuthEmulator } from "firebase/auth";
 import firebase from "firebase/compat/app";
 
 // TODO: Replace the following with your app's Firebase project configuration
-var firebaseConfig = {
+firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-};
-firebase.initializeApp(firebaseConfig);
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+});
 
 if (process.env.NODE_ENV === "development") {
   const auth = getAuth();
